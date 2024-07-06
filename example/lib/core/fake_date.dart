aafb3a48b8941eba90705aef2396140014d5e609
Mustafa Ibrahim <Mustafa7ibrahim@gmail.com>
2024-07-06 23:29:20 +0300

    feat: Add fake date list for testing purposes

---
+++ example/lib/core/fake_date.dart
@@ -0,0 +1,38 @@
+final List<String> item1 = [
+  'Item Internal Mr',
+  'Item Senior Qua',
+  'Item Future Impervisor',
+  'Item National M',
+  'Item District Q',
+  'Item Corporate Executive',
+  'Item Forward Met',
+  'Item District Rnt',
+  'Item Internal Sant',
+  'Item Global Imp',
+];
+
+final List<String> items2 = [
+  'Item Internal Mobility Designer',
+  'Item Senior Quality Specialist',
+  'Item Future Implementation Supervisor',
+  'Item National Markets Designer',
+  'Item District Quality Designer',
+  'Item Corporate Communications Executive',
+  'Item Forward Metrics Strategist',
+  'Item District Research Assistant',
+  'Item Internal Security Consultant',
+  'Item Global Implementation Producerh',
+];
+
+final List<String> items3 = [
+  'Item Virgin Islands, U.S.',
+  'Item French Southern Territories',
+  'Item American Samoa',
+  'Item Northern Mariana Islands',
+  'Item Bosnia and Herzegovina',
+  'Item Turks and Caicos Islands',
+  'Item Bangladesh',
+  'Item Lebanon',
+  'Item Mongolia',
+  'Item this is a very long item to test the enhanced deduplication function',
+];
