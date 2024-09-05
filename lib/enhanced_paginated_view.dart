a0a53fea3f7bbd6a9176a34441652b89342c33db
Mustafa Ibrahim <Mustafa7ibrahim@gmail.com>
2024-09-05 18:32:48 +0300

    Refactor import statements for error load more and error page configurations

--- lib/enhanced_paginated_view.dart
+++ lib/enhanced_paginated_view.dart
@@ -1,17 +1,13 @@
 library enhanced_paginated_view;
 
-// ======== MAIN ======== //
 export 'package:enhanced_paginated_view/src/enhanced_paginated_view.dart';
 
-// ======== CORE ======== //
 export 'package:enhanced_paginated_view/src/core/enhanced_deduplication.dart';
 export 'package:enhanced_paginated_view/src/models/enhanced_delegate.dart';
 
-// ======== ENUMS ======== //
 export 'package:enhanced_paginated_view/src/models/enhanced_status.dart';
 export 'package:enhanced_paginated_view/src/models/enhanced_view_direction.dart';
 
-// ======== CONFIGS ======== //
 export 'package:enhanced_paginated_view/src/models/error_load_more_config.dart';
 export 'package:enhanced_paginated_view/src/models/empty_widget_config.dart';
 export 'package:enhanced_paginated_view/src/models/error_page_config.dart';
