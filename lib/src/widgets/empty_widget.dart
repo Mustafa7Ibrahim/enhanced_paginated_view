d978f152663e4c828bc7cfe0642ea19b6e7d3227
Mustafa Ibrahim <Mustafa7ibrahim@gmail.com>
2024-09-05 18:24:18 +0300

    refactor: Update EmptyWidget to use custom configuration

--- lib/src/widgets/empty_widget.dart
+++ lib/src/widgets/empty_widget.dart
@@ -1,5 +1,7 @@
+import 'package:enhanced_paginated_view/src/models/empty_widget_config.dart';
 import 'package:enhanced_paginated_view/src/models/enhanced_view_type.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 /// A widget that represents an empty state.
 ///
@@ -9,17 +11,20 @@ class EmptyWidget extends StatelessWidget {
   /// The type of enhanced view.
   final EnhancedViewType enhancedViewType;
 
+  /// the config for the empty widget
+  final EmptyWidgetConfig config;
+
   /// Creates an `EmptyWidget` with the specified [enhancedViewType].
-  const EmptyWidget._(this.enhancedViewType);
+  const EmptyWidget._(this.enhancedViewType, this.config);
 
   /// Creates an `EmptyWidget` with a box view type.
-  factory EmptyWidget() {
-    return const EmptyWidget._(EnhancedViewType.box);
+  factory EmptyWidget({required EmptyWidgetConfig config}) {
+    return EmptyWidget._(EnhancedViewType.box, config);
   }
 
   /// Creates an `EmptyWidget` with a sliver view type.
-  factory EmptyWidget.sliver() {
-    return const EmptyWidget._(EnhancedViewType.sliver);
+  factory EmptyWidget.sliver({required EmptyWidgetConfig config}) {
+    return EmptyWidget._(EnhancedViewType.sliver, config);
   }
 
   @override
@@ -39,13 +44,49 @@ class EmptyWidget extends StatelessWidget {
 
   /// Builds the content with a box view type.
   Widget _buildBox(BuildContext context) {
-    return const SizedBox();
+    return config.customWidget ?? _EmptyWidget(config);
   }
 
   /// Builds the content with a sliver view type.
   Widget _buildSliver(BuildContext context) {
-    return const SliverToBoxAdapter(
-      child: SizedBox(),
+    return config.customWidget ??
+        SliverToBoxAdapter(child: _EmptyWidget(config));
+  }
+}
+
+class _EmptyWidget extends StatelessWidget {
+  const _EmptyWidget(this.config);
+
+  final EmptyWidgetConfig config;
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.8,
+              child: AspectRatio(
+                aspectRatio: 1,
+                child: SvgPicture.string(
+                  config.svgIcon,
+                  fit: BoxFit.scaleDown,
+                ),
+              ),
+            ),
+            Text(
+              config.title,
+              style: Theme.of(context)
+                  .textTheme
+                  .headlineSmall!
+                  .copyWith(fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
